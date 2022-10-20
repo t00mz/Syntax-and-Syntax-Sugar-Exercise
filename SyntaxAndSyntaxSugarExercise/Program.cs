2cfbf589d995b6c3506f1637040a98d2f10d2440
Michael Hatoum <hatoumm89@gmail.com>
2022-10-20 12:45:06 -0400

    done

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+
+            //inferred typing
+            //string interpolation
+            //and the ternary operator
+
+
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than or equal to 9";
+
+          Console.WriteLine(response);
         }
     }
 }
+
